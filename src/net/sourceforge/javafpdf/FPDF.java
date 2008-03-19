a8af3f8788af5808bfb1132d46f850e5ff5690c8
Ashmo Dai <ashmodai@sourceforge.net>
2008-03-19 18:40:49 +0000

    git-svn-id: svn://svn.code.sf.net/p/javafpdf/code/trunk@9 abe4f3f4-cd48-0410-bc55-8ea64a3f98cb

--- src/net/sourceforge/javafpdf/FPDF.java
+++ src/net/sourceforge/javafpdf/FPDF.java
@@ -826,7 +826,7 @@ public abstract class FPDF {
 
 	protected void _putinfo() {
 		this._out("/Producer " //$NON-NLS-1$
-				+ this._textstring("Java FPDF 1.53 / " //$NON-NLS-1$
+				+ this._textstring("Java FPDF 1.53." //$NON-NLS-1$
 						+ revision));
 		if (this.title != null) {
 			this._out("/Title " + this._textstring(this.title)); //$NON-NLS-1$
